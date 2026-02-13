e94ae9c9d429b15c4abcae7818464f1e9033c34f
stanleymauricio <stanleymauricio.sm@gmail.com>
2026-02-12 23:42:06 -0500

    added

--- js/utils.mjs
+++ js/utils.mjs
@@ -2,7 +2,7 @@
 async function getTemplate(path) {
   const response = await fetch(path);
   if (!response.ok) {
-    throw new Error(`No se pudo encontrar el template en: ${path}`);
+    throw new Error(`N: ${path}`);
   }
   const text = await response.text();
   return text;
@@ -15,20 +15,20 @@ export async function loadHeaderFooter() {
   const footerPath = "public/partials/footer.html";
 
   try {
-    // Descargamos los templates
+
     const headerTemplate = await getTemplate(headerPath);
     const footerTemplate = await getTemplate(footerPath);
 
-    // Buscamos los contenedores en el HTML
+   
     const headerElement = document.querySelector("#main-header");
     const footerElement = document.querySelector("#main-footer");
 
-    // Inyectamos el contenido
+  
     if (headerElement) headerElement.innerHTML = headerTemplate;
     if (footerElement) footerElement.innerHTML = footerTemplate;
 
-    console.log("Header y Footer cargados correctamente.");
+    console.log("Header and footer ");
   } catch (error) {
-    console.error("Error al cargar los componentes (partials):", error);
+    console.error("Error", error);
   }
 }
